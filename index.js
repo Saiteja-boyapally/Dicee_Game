5e45b139f5760feb3b1890b9a06290eeae98c2bf
SAI_TEJA_BOYAPALLY <40123986+Saiteja-boyapally@users.noreply.github.com>
2021-11-22 09:46:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var diceImage1 = "images/dice" + randomNumber1 + ".png"
+
+var diceImage2 = "images/dice" + randomNumber2 + ".png"
+
+document.querySelector("button").addEventListener("click",function()
+{
+
+  document.querySelector("button").innerHTML = "PLAY"
+  console.log(diceImage1);
+  document.querySelector(".img1").setAttribute("src",diceImage1);
+  document.querySelector(".img2").setAttribute("src",diceImage2);
+
+  if(randomNumber1>randomNumber2)
+  {
+    document.querySelector("h1").innerHTML = "⛳ Player 1 Wins"
+  }
+  else
+  if(randomNumber1<randomNumber2)
+  {
+    document.querySelector("h1").innerHTML = "Player 2 Wins ⛳"
+  }
+  else
+  {
+    document.querySelector("h1").innerHTML = "Draw"
+  }
+  document.querySelector("button").innerHTML = "Refresh to start";
+
+});
